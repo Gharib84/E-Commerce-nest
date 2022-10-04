db6ad40747cd3486330c0020161e172af2dafdac
gharib84 <itprofessional63@gmail.com>
2022-10-04 16:28:26 +0200

    about page has finished

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Render } from '@nestjs/common';
+import { rename } from 'fs';
 
 
 @Controller()
@@ -10,6 +11,20 @@ export class AppController {
   index() {
 
   }
+
+ @Get("/about")
+ @Render('about')
+ about(){
+    let aboutInfo:{} = {
+      info: "This is Simple Store app Created By me. This App Depend on typescript programing language as backend by using nest framework ",
+      cardText: "About Page",
+      cardFooter: "Created By Mahmoud Eddie"
+    }
+
+    return {
+      about: aboutInfo
+    }
+ }
 
 
 }
